9797a0f47d36625dade4d68a1bf5611df1582a22
Gang Wu <gokure@gmail.com>
2025-06-17 17:05:52 +0800

    Change configs.

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -179,7 +179,7 @@ export const MAP_TILE_STYLE = 'dark-v10';
 // access token. you can apply a new one, it's free.
 // maptiler: Gt5R0jT8tuIYxW6sNrAg | sign up at https://cloud.maptiler.com/auth/widget
 // stadiamaps: 8a769c5a-9125-4936-bdcf-a6b90cb5d0a4 |sign up at https://client.stadiamaps.com/signup/
-export const MAP_TILE_ACCESS_TOKEN = 'Gt5R0jT8tuIYxW6sNrAg';
+export const MAP_TILE_ACCESS_TOKEN = '8lDyDF0TWYCn4LYzGYbq';
 
 export const MAP_TILE_STYLES = {
   maptiler: {
